2640684564107410d23de84583d6a224f2e6c7ae
simrankaurkhaira1467 <39944481+simrankaurkhaira1467@users.noreply.github.com>
2018-06-06 14:42:37 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,28 @@
+#question 1 To print anything you want on screen.
+print("Let's get started with python")
+
+#question 2 Join two strings using '+'.
+print("Acad" + "View")
+
+#question 3 Take input of 3 variables x,y and z.Print their values on screen.
+x = input("enter x:")
+print(x)
+y = input("enter y:")
+print(y)
+z =input("enter z:")
+print(z)
+
+#question 4 Print "let's get started" on screen.
+print("Let's get started")
+
+#question 5 Print the given values using placeholders.
+s ="Acadview"
+course ="Python"
+fees =5000
+print("s:%s\n course:%s\n fees:%d" %(s,course,fees))
+
+#question 6 To do an interesting exercise.
+name="Tony Stark"
+salary=1000000
+print("%s %d" %(name,salary))
+
